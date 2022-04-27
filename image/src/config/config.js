75d9363ab932297ec61b6beaf0102a37ecdedd71
kubagrzelak <jakubgrzelak1@gmail.com>
2022-04-27 13:58:09 +0200

    Changed from Input to Process module.

--- image/src/config/config.js
+++ image/src/config/config.js
@@ -2,8 +2,8 @@ const env = require('../utils/env')
 
 module.exports = {
   EGRESS_URL: env('EGRESS_URL', 'https://testmp.free.beeceptor.com/'),
-  HOST_NAME: env('HOST_NAME', '127.0.0.1'),
-  HOST_PORT: env('HOST_PORT', '8080'),
+  INGRESS_HOST: env('INGRESS_HOST', '127.0.0.1'),
+  INGRESS_PORT: env('INGRESS_PORT', '8080'),
   NESTED_RESPONSE: env('NESTED_RESPONSE', 'no'),
   CUSTOM_FIELDS: env('CUSTOM_FIELDS', ''),
   MODULE_NAME: env('MODULE_NAME', 'McClimate Vicki Decoder'),
