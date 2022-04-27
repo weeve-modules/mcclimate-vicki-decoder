dcf9ff70377ea08770827dad21fb52bb0e9f2d15
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-04-27 19:23:43 +0200

    removed temp url

--- image/src/config/config.js
+++ image/src/config/config.js
@@ -1,7 +1,7 @@
 const env = require('../utils/env')
 
 module.exports = {
-  EGRESS_URL: env('EGRESS_URL', 'https://testmp.free.beeceptor.com/'),
+  EGRESS_URL: env('EGRESS_URL', ''),
   INGRESS_HOST: env('INGRESS_HOST', '127.0.0.1'),
   INGRESS_PORT: env('INGRESS_PORT', '8080'),
   NESTED_RESPONSE: env('NESTED_RESPONSE', 'no'),
